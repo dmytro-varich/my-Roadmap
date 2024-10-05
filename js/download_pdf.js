7d29a1afb6a5e8a53229236430efb2cfc5498fe0
Dmytro Varich <136006220+dmytro-varich@users.noreply.github.com>
2024-10-05 15:48:36 +0200

    Fixed

--- js/download_pdf.js
+++ js/download_pdf.js
@@ -1,3 +1,13 @@
 document.getElementById('pdfButton').addEventListener('click', function() {
+    document.querySelectorAll("textarea").forEach(function(textarea) {
+        textarea.style.height = textarea.scrollHeight + "px" ;
+        textarea.style.overflowY = "hidden";
+
+        textarea.addEventListener("input", function() {
+            this.style.height = "auto";
+            this.style.height = this.scrollHeight + "px";
+        });
+    });
+    
     window.print();
 });
